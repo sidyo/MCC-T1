f8ce241150aee9388091378c8ec0e38671d4d4e5
Vinicius Bressan <vinitb12@hotmail.com>
2019-06-17 04:34:34 -0300

    Distributed States Implemented

--- src/client.go
+++ src/client.go
@@ -2,159 +2,139 @@ package main
 
 import (
 	"fmt"
-	//"sync"
+	"os"
+	"strconv"
+	"strings"
 	"time"
-	"bufio"
-    	"os"
+
+	BEB "../BestEffortBroadcast"
 	term "../termbox"
-	//runewidth "../runewidth"
-    	BEB "../BestEffortBroadcast"
-	//term "github.com/nsf/termbox-go"
 )
-
-var reader = bufio.NewReader(os.Stdin)
-
-const maxPlayers = 3
-const tabTamanho = 10
-
-var tab = montarTabuleiro()
-var pl = playerInit()
 
 type player struct {
 	x, y int
 }
 
-type celula struct {
-	top, bottom, left, right bool
+const maxPlayers = 4
+const boardSize = 10
+const currentPlayers = 2
+const bombExplosionTime = 3 * time.Second
+const fireFadeTime = 500 * time.Millisecond
+
+var basePositions = []player{
+	player{
+		x: 1,
+		y: 1,
+	},
+	player{
+		x: boardSize - 2,
+		y: boardSize - 2,
+	},
+	player{
+		x: boardSize - 2,
+		y: 0,
+	},
+	player{
+		x: 0,
+		y: boardSize - 2,
+	},
+}
+var chAction chan string = make(chan string)
+var chUpdateGame chan string = make(chan string)
+var address string
+var players [maxPlayers]player
+var board [boardSize][boardSize]bool
+var bombs [boardSize][boardSize]bool
+var fires [boardSize][boardSize]int
+
+func playerInit() {
+	for i := 0; i < currentPlayers; i++ {
+		players[i] = basePositions[i]
+	}
 }
 
-type tabuleiro struct {
-	tab [tabTamanho][tabTamanho]celula
-}
-
-func playerInit() player {
-	p := player{}
-	p.x = 0
-	p.y = 0
-	return p
-}
-
-func montarTabuleiro() tabuleiro {
-	tab := tabuleiro{}
-	for i := 0; i < tabTamanho; i++ {
-		for j := 0; j < tabTamanho; j++ {
-			cel := celula{}
-
-			cel.top = true
-			cel.bottom = true
-			cel.left = true
-			cel.right = true
-
-			if j == 0 {
-				cel.top = false
-			} else if j == tabTamanho - 1 {
-				cel.bottom = false
+func buildBoard() {
+	for j := 0; j < boardSize; j++ {
+		for i := 0; i < boardSize; i++ {
+			if j == 0 || j == boardSize-1 || i == 0 || i == boardSize-1 {
+				board[j][i] = true
+			} else {
+				board[j][i] = false
 			}
-
-			if i == 0 {
-				cel.left = false
-			} else if i == tabTamanho - 1 {
-				cel.right = false
-			}
-
-			tab.tab[i][j] = cel
 		}
 	}
-	return tab
 }
 
-func reset() {
-        //term.Sync() // cosmestic purpose
-        printGame()
+func resetScreen() {
+	term.Sync() // cosmestic purpose
+	printGame()
 }
 
-func keyListener(){
+func keyListener() {
 	err := term.Init()
 	if err != nil {
-		 panic(err)
+		panic(err)
 	}
 
 	defer term.Close()
 
-	fmt.Println("Enter any key to see their ASCII code or press ESC button to quit")
-
-	keyPressListenerLoop:
-		for {
-			switch ev := term.PollEvent(); ev.Type {
-			case term.EventKey:
-				switch ev.Key {
-					case term.KeyEsc:
-						break keyPressListenerLoop
-					case term.KeyArrowUp:
-						move(2)
-						reset()
-						fmt.Println("Arrow Up pressed")
-					case term.KeyArrowDown:
-						move(4)
-						reset()
-						fmt.Println("Arrow Down pressed")
-
-					case term.KeyArrowLeft:
-						move(3)
-						reset()
-						fmt.Println("Arrow Left pressed")
-
-					case term.KeyArrowRight:
-						move(1)
-						reset()
-						fmt.Println("Arrow Right pressed")
-
-					case term.KeySpace:
-						bomb()
-						reset()
-						fmt.Println("Space pressed")
-					default:
-						// we only want to read a single character or one key pressed event
-						reset()
-						fmt.Println("ASCII : ", ev.Ch)
-				}
-			case term.EventError:
-				 panic(ev.Err)
+keyPressListenerLoop:
+	for {
+		switch ev := term.PollEvent(); ev.Type {
+		case term.EventKey:
+			switch ev.Key {
+			case term.KeyEsc:
+				fmt.Println("Game Closed!")
+				break keyPressListenerLoop
+			case term.KeyArrowUp:
+				//fmt.Println("Arrow Up pressed")
+				chAction <- "move " + address + " up"
+			case term.KeyArrowDown:
+				//fmt.Println("Arrow Down pressed")
+				chAction <- "move " + address + " down"
+			case term.KeyArrowLeft:
+				//fmt.Println("Arrow Left pressed")
+				chAction <- "move " + address + " left"
+			case term.KeyArrowRight:
+				//fmt.Println("Arrow Right pressed")
+				chAction <- "move " + address + " right"
+			case term.KeySpace:
+				//fmt.Println("Space pressed")
+				chAction <- "bomb " + address
+			default:
+				// we only want to read a single character or one key pressed event
+				//fmt.Println("ASCII : ", ev.Ch)
 			}
+		case term.EventError:
+			panic(ev.Err)
 		}
-}
-
-func bomb() {
-	fmt.Printf("Bomb planted at: %d, %d\n",pl.x,pl.y)
-	time.Sleep(2000000000)
-	fmt.Print("Booom!\n")
-}
-
-func move(action int) {
-	switch action {
-		case 1:
-			if(pl.x < tabTamanho - 1) {
-				pl.x = pl.x + 1
-			}
-		case 2:
-			if(pl.y > 0) {
-				pl.y = pl.y - 1
-			}
-		case 3:
-			if(pl.x > 0) {
-				pl.x = pl.x - 1
-			}
-		case 4:
-			if(pl.y < tabTamanho - 1) {
-				pl.y = pl.y + 1
-			}
-		default:
-			fmt.Print("move default\n")
 	}
 }
 
-func printGame () {
-	fmt.Printf("Player position: %d, %d\n", pl.x, pl.y)
+func printGame() {
+	for j := 0; j < boardSize; j++ {
+		for i := 0; i < boardSize; i++ {
+			playerpos := false
+			if board[i][j] {
+				fmt.Print("# ")
+			} else if bombs[i][j] {
+				fmt.Print("@ ")
+			} else if fires[i][j] > 0 {
+				fmt.Print("* ")
+			} else {
+				for p := 0; p < currentPlayers; p++ {
+					if players[p].x == i && players[p].y == j {
+						fmt.Printf("%v ", p+1)
+						playerpos = true
+					}
+				}
+				if !playerpos {
+					fmt.Print("  ")
+				}
+			}
+		}
+		fmt.Printf("\n")
+	}
 }
 
 func networkinit() {
@@ -164,6 +144,7 @@ func networkinit() {
 		return
 	}
 
+	address = os.Args[1]
 	addresses := os.Args[1:]
 	fmt.Println(addresses)
 
@@ -176,27 +157,19 @@ func networkinit() {
 	// enviador de broadcasts
 	go func() {
 
-		scanner := bufio.NewScanner(os.Stdin)
-		var msg string
-
 		for {
-			if scanner.Scan() {
-				msg = scanner.Text()
-			}
+			msg := <-chAction
 			req := BEB.BestEffortBroadcast_Req_Message{
 				Addresses: addresses[1:],
 				Message:   msg}
 			beb.Req <- req
 		}
 	}()
-
 	// receptor de broadcasts
 	go func() {
 		for {
-
 			in := <-beb.Ind
-			fmt.Printf("Message from %v: %v\n", in.From, in.Message)
-
+			chUpdateGame <- in.Message
 		}
 	}()
 
@@ -204,10 +177,138 @@ func networkinit() {
 	<-blq
 }
 
+func updateGame() {
+	for {
+		msg := <-chUpdateGame
+		aux := strings.Split(msg, " ")
+		if aux[0] == "bomb" {
+			player := aux[1]
+			size := aux[2]
+			bomb(player, size)
+		} else if aux[0] == "move" {
+			player := aux[1]
+			direction := aux[2]
+			move(player, direction)
+		} else {
+			fmt.Println("MESSAGE ERROR!")
+		}
+	}
+}
+
+func bomb(playerId string, size string) {
+	p, _ := strconv.Atoi(playerId)
+	p = p - 1
+	s, _ := strconv.Atoi(size)
+	x := players[p].x
+	y := players[p].y
+	go bombAux(x, y, s)
+}
+
+func bombAux(x int, y int, size int) {
+	bombs[x][y] = true
+	resetScreen()
+	time.Sleep(2 * time.Second)
+	bombs[x][y] = false
+	resetScreen()
+	fires[x][y]++
+	for i := 1; i <= size; i++ {
+		if x-i > 0 {
+			fires[x-i][y]++
+		}
+		if y-i > 0 {
+			fires[x][y-i]++
+		}
+		if x+i < boardSize {
+			fires[x+i][y]++
+		}
+		if y+i < boardSize {
+			fires[x][y+i]++
+		}
+	}
+	resetScreen()
+	hitDetection()
+	time.Sleep(fireFadeTime)
+	fires[x][y]--
+	for i := 1; i <= size; i++ {
+		if x-i > 0 {
+			fires[x-i][y]--
+		}
+		if y-i > 0 {
+			fires[x][y-i]--
+		}
+		if x+i < boardSize {
+			fires[x+i][y]--
+		}
+		if y+i < boardSize {
+			fires[x][y+i]--
+		}
+	}
+	resetScreen()
+}
+
+func hitDetection() {
+	for i := 0; i < currentPlayers; i++ {
+		x := players[i].x
+		y := players[i].y
+		if fires[x][y] > 0 {
+			fmt.Printf("PLAYER %v TOOK DAMAGE!\n", i+1)
+		}
+	}
+}
+
+func move(playerId string, direction string) {
+	p, _ := strconv.Atoi(playerId)
+	p = p - 1
+	x := players[p].x
+	y := players[p].y
+	collision := false
+	xUpd := x
+	yUpd := y
+	switch direction {
+	case "up":
+		collision = checkCollision(x, y-1)
+		yUpd = yUpd - 1
+	case "down":
+		collision = checkCollision(x, y+1)
+		yUpd = yUpd + 1
+	case "left":
+		collision = checkCollision(x-1, y)
+		xUpd = xUpd - 1
+	case "right":
+		collision = checkCollision(x+1, y)
+		xUpd = xUpd + 1
+	}
+	if !collision {
+		players[p].x = xUpd
+		players[p].y = yUpd
+		resetScreen()
+	}
+}
+
+func checkCollision(x int, y int) bool {
+	if x < 0 || y < 0 {
+		return true
+	}
+	playerPos := false
+	for p := 0; p < currentPlayers; p++ {
+		if players[p].x == x && players[p].y == y {
+			playerPos = true
+		}
+	}
+	return playerPos || board[x][y] || bombs[x][y]
+}
 func main() {
-	printGame()
+	go networkinit()
+	go updateGame()
+	buildBoard()
+	playerInit()
 	go keyListener()
+	time.Sleep(1000000000)
+	resetScreen()
 
 	var block chan int = make(chan int)
 	<-block
 }
+
+//go run client.go 127.0.0.1:2001 127.0.0.1:1001
+//go run client.go 127.0.0.1:3001 127.0.0.1:1001
--- src/server.go
+++ src/server.go
@@ -1,176 +1,31 @@
 package main
 
 import (
-    "fmt"
-    //"sync"
-    "time"
-    "bufio"
-    "os"
-    BEB "../BestEffortBroadcast"
-    "strings"
-    "strconv"
+	"fmt"
+	//"sync"
+
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	BEB "../BestEffortBroadcast"
 )
 
-// -----------------------------------------
-//
-// -----------------------------------------
+var addresses []string
+var chAction chan string = make(chan string)
 
-type Semaphore struct {
-	inc, dec chan struct{}
-	val      int
-}
+const bombSize = 3
+const maxPlayers = 3
+const boardSize = 10
 
-func NewSemaphore(v int) *Semaphore {
-	s := &Semaphore{
-		inc: make(chan struct{}),
-		dec: make(chan struct{}),
-		val: v}
-
-	go func() {
-		for {
-			if s.val == 0 {
-				<-s.inc
-				s.val++
-			}
-			if s.val > 0 {
-				select {
-				case <-s.inc:
-					s.val++
-				case <-s.dec:
-					s.val--
-				}
-			}
+func findPlayer(address string) int {
+	for i := 0; i < len(addresses); i++ {
+		if addresses[i] == address {
+			return i
 		}
-	}()
-	return s
-}
-
-func (s *Semaphore) Signal() {
-	s.inc <- struct{}{}
-}
-
-func (s *Semaphore) Wait() {
-	s.dec <- struct{}{}
-}
-
-type Monitor struct {
-	mutex      *Semaphore // garante exclusão mutua do monitor
-	next       *Semaphore // bloqueia thread que sinaliza em favor de outra - vide signal de condition
-	next_count int        // conta threads em next, que podem prossegir
-}
-
-func initMonitor() *Monitor {
-	m := &Monitor{
-		mutex:      NewSemaphore(1),
-		next:       NewSemaphore(0),
-		next_count: 0}
-
-	return m
-}
-
-//  procedimentos do monitor
-
-func (m *Monitor) monitorEntry() {
-	m.mutex.Wait() // entrada no monitor ee so passar pelo mutex
-}
-
-func (m *Monitor) monitorExit() {
-	if m.next_count > 0 { // libera uma thread que ja esteve no monitor, senao libera mutex
-		m.next.Signal()
-	} else {
-		m.mutex.Signal()
 	}
-}
-
-//  estruturas genericas de  variaveis condicao
-
-type Condition struct {
-	s     *Semaphore // semaforo para bloquear na condicao
-	count int        // contador de bloqueados
-	m     *Monitor   // monitor associado aa condicao - quando bloqueia na condicao libera o monitor (next ou mutex)
-	name  string
-}
-
-func initCondition(n string, m1 *Monitor) *Condition {
-	c := &Condition{
-		s:     NewSemaphore(0), // 0 inicia bloqueando
-		count: 0,               // contadores de bloqueados nesta condicao
-		m:     m1,              // o monitor associado
-		name:  n}
-
-	return c
-}
-
-//  procedimentos de variaveis condicao
-
-func (c *Condition) condWait() {
-	// fmt.Println("                                           wait  ", c.name)
-	c.count++         // mais uma thread vai bloquear aqui nesta condition
-	c.m.monitorExit() // libera o monitor associado aa condition
-	c.s.Wait()        // bloqueia !!     fica aqui ate alguem dar signal  !!
-	c.count--         // esta linha é executada depois de alguem ter dado signal, entao um bloqueado a menos
-}
-
-func (c *Condition) condSignal() {
-	if c.count > 0 { // tem alguem para sinalizar ?    se nao tem entao nao faz nada, signal nao tem efeito!
-		c.m.next_count++ // opa, tem alguem para sinalizar, entao esta thread se bloqueia em favor da sinalizada
-		c.s.Signal()
-		c.m.next.Wait()
-		c.m.next_count-- // foi desbloqueada (veja monitorExit), aqui desbloqueou, decrementa
-	}
-}
-
-// -----------------------------------------
-//
-// -----------------------------------------
-
-func buildBoard() {
-    for j := 0; j < boardSize; j++ {
-        for i := 0; i < boardSize; i++ {
-            if j == 0 || j == boardSize - 1 || i == 0 || i == boardSize - 1 {
-                board[j][i] = true
-            } else {
-                board[j][i] = false
-            }
-        }
-    }
-}
-
-func playerInit() [2]player {
-    p := [2]player{player{}, player{}}
-
-    p[0].x = 1
-    p[0].y = 1
-
-    p[1].x = boardSize - 2
-    p[1].y = boardSize - 2
-
-    return p
-}
-
-func move(player int, action int) {
-	switch action {
-		case 1:
-			if(pl[player].x < boardSize - 2) {
-				pl[player].x = pl[player].x + 1
-			}
-		case 2:
-			if(pl[player].y > 1) {
-				pl[player].y = pl[player].y - 1
-			}
-		case 3:
-			if(pl[player].x > 1) {
-				pl[player].x = pl[player].x - 1
-			}
-		case 4:
-			if(pl[player].y < boardSize - 2) {
-				pl[player].y = pl[player].y + 1
-			}
-		default:
-			fmt.Print("move default\n")
-	}
-    go hitDetection()
-    go printGame()
+	return -1
 }
 
 func networkinit() {
@@ -180,7 +35,7 @@ func networkinit() {
 		return
 	}
 
-	addresses := os.Args[1:]
+	addresses = os.Args[1:]
 	fmt.Println(addresses)
 
 	beb := BEB.BestEffortBroadcast_Module{
@@ -192,13 +47,8 @@ func networkinit() {
 	// enviador de broadcasts
 	go func() {
 
-		scanner := bufio.NewScanner(os.Stdin)
-		var msg string
-
 		for {
-			if scanner.Scan() {
-				msg = scanner.Text()
-			}
+			msg := <-chAction
 			req := BEB.BestEffortBroadcast_Req_Message{
 				Addresses: addresses[1:],
 				Message:   msg}
@@ -210,19 +60,8 @@ func networkinit() {
 	go func() {
 		for {
 			in := <-beb.Ind
-            if strings.HasPrefix(in.Message, "bomb") {
-                aux := strings.Split(in.Message, " ")
-                x, _ := strconv.Atoi(aux[1])
-                y, _ := strconv.Atoi(aux[2])
-                size, _ := strconv.Atoi(aux[3])
-                go bomb(x, y, size)
-            } else if strings.HasPrefix(in.Message, "move") {
-                aux := strings.Split(in.Message, " ")
-                player, _ := strconv.Atoi(aux[1])
-                direction, _ := strconv.Atoi(aux[2])
-                go move(player, direction)
-            }
-			//fmt.Printf("Message from %v: %v\n", in.From, in.Message)
+			fmt.Println("Received: " + in.Message)
+			chAction <- treatMessage(in.Message)
 		}
 	}()
 
@@ -230,116 +69,22 @@ func networkinit() {
 	<-blq
 }
 
-func printMonitorInit() *printMonitor {
-	mon := initMonitor()
-	mbc := &printMonitor{
-		m:     mon,
-		cond: initCondition("test", mon),
+func treatMessage(msg string) string {
+	aux := strings.Split(msg, " ")
+	var playerId int
+	if aux[0] == "move" {
+		playerId = findPlayer(aux[1])
+		return aux[0] + " " + strconv.Itoa(playerId) + " " + aux[2]
+	} else if aux[0] == "bomb" {
+		playerId = findPlayer(aux[1])
+		return aux[0] + " " + strconv.Itoa(playerId) + " " + strconv.Itoa(bombSize)
 	}
-	return mbc
+	return "The server received an invalid message!"
 }
-
-func printGame () {
-    pMonitor.m.monitorEntry()
-    for j := 0; j < boardSize; j++ {
-        for i := 0; i < boardSize; i++ {
-            if board[j][i] {
-                fmt.Printf("# ")
-            } else if bombs[j][i] {
-                fmt.Printf("@ ")
-            } else if fires[j][i] > 0 {
-                fmt.Printf("* ")
-            } else if pl[0].x == i && pl[0].y == j {
-                fmt.Printf("1 ")
-            } else if pl[1].x == i && pl[1].y == j {
-                fmt.Printf("2 ")
-            } else {
-                fmt.Printf("  ")
-            }
-        }
-        fmt.Printf("\n")
-    }
-    pMonitor.m.monitorExit()
-}
-
-func bomb(x int, y int, size int) {
-    bombs[x][y] = true
-    go printGame()
-    //fmt.Printf("Bomb planted at: %d, %d\n",pl.x,pl.y)
-    time.Sleep(2000000000)
-    //fmt.Print("Booom!\n")
-    bombs[x][y] = false
-
-    fires[x][y]++
-    for i := 1; i <= size; i++ {
-        if x - i > 0 { fires[x - i][y]++ }
-        if y - i > 0 { fires[x][y - i]++ }
-        if x + i < boardSize { fires[x + i][y]++ }
-        if y + i < boardSize { fires[x][y + i]++ }
-    }
-
-    go hitDetection()
-    go printGame()
-
-    time.Sleep(500000000)
-
-    fires[x][y]--
-    for i := 1; i <= size; i++ {
-        if x - i > 0 { fires[x - i][y]-- }
-        if y - i > 0 { fires[x][y - i]-- }
-        if x + i < boardSize { fires[x + i][y]-- }
-        if y + i < boardSize { fires[x][y + i]-- }
-    }
-
-    go printGame()
-}
-
-func hitDetection() {
-    pMonitor.m.monitorEntry()
-    x1 := pl[0].x
-    y1 := pl[0].y
-    if fires[y1][x1] > 0 {
-        fmt.Println("PLAYER 1 IS DEAD!")
-    }
-
-    x2 := pl[1].x
-    y2 := pl[1].y
-    if fires[y2][x2] > 0 {
-        fmt.Println("PLAYER 2 IS DEAD!")
-    }
-    pMonitor.m.monitorExit()
-}
-
-type printMonitor struct {
-	// sincronizacao
-	m     *Monitor
-	cond *Condition
-}
-
-type player struct {
-    x, y int
-}
-
-var pMonitor = printMonitorInit()
-
-var reader = bufio.NewReader(os.Stdin)
-
-const maxPlayers = 3
-const boardSize = 10
-
-var board [boardSize][boardSize]bool
-var bombs [boardSize][boardSize]bool
-var fires [boardSize][boardSize]int
-
-var pl [2]player = playerInit()
 
 func main() {
-    go networkinit()
-    go buildBoard()
-    go printGame()
-    time.Sleep(2000000000)
-    go bomb(3,4,2)
-    go bomb(4,4,4)
-    var block chan int = make(chan int)
-    <-block
+	go networkinit()
+	time.Sleep(1000000000)
+	var block chan int = make(chan int)
+	<-block
 }
